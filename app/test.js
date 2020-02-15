9c2763342d360605eb7f2c1842d9dc664d6dd729
Wojciech <olowek92@gmail.com>
2020-02-15 22:38:37 +0100

    Several tests done

--- app/test.js
+++ app/test.js
@@ -1,1 +1,27 @@
-console.log('Wygląda na to, że wszystko działa :)');
+// TEST 1
+
+// setTimeout(() => {
+//   console.log("Wygląda na to, że wszystko działa :)");
+// }, 2000);
+
+// TEST 2
+
+const text = "Hello, World!";
+
+let counter = 0;
+//Program wyświetla po znaku z tekstu...
+const intervalId = setInterval(() => {
+  console.log(text[counter]);
+  counter++;
+
+  //...ręcznie musimy mu powiedzieć kiedy należy przerwać wykonywanie - wtedy proces Node.js się zakończy
+  if (counter === text.length) {
+    clearInterval(intervalId);
+  }
+}, 400);
+
+// TEST 3
+
+// process.argv.forEach((val, index) => {
+//     console.log(`${index}: ${val}`);
+//   });
