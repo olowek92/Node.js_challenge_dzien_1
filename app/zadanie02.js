e3e1cb6935505e04d611d5beff5c61bd74d9c0b2
Wojciech <olowek92@gmail.com>
2020-02-15 23:06:16 +0100

    Second task done

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,16 @@
-//Twój kod
+const messages = {
+  4: "Node.js",
+  1: "się",
+  0: "Witam",
+  6: "i korzystam",
+  5: "w konsoli",
+  7: "z funkcji czasu!",
+  2: "z",
+  3: "programem"
+};
+
+for (let i in messages) {
+  setTimeout(() => {
+    console.log(messages[i]);
+  }, i * 1000);
+}
